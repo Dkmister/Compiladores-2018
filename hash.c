665d37cca10da5e47bcdd28bfe699b892be9fc7d
Vilmar Neto <vilmar.neto@gmail.com>
2018-03-13 16:52:07 -0300

    Added hash code

---
+++ hash.c
@@ -0,0 +1,63 @@
+#include "hash.h"
+
+HASH* Table[HASH_SIZE];
+
+void hashInit(void)
+{
+  int i ;
+   
+  for (i=0;i<HASH_SIZE;++i)
+     Table[i] = 0 ;
+}
+
+
+
+int hashAddress(char *text){
+
+  int address = 1;
+
+  int i ;
+
+  for(i=0;i<strlen(text);i++)
+     address = ( address * text[i] ) % HASH_SIZE + 1 ;
+
+  return address - 1;
+}
+
+
+
+HASH* hashInsert(int type, char *text)
+{
+  int address;
+
+  HASH *newnode = 0;
+
+  adress = hashAddress(text);
+
+  newnode = (HASH*) calloc(1,sizeof(HASH);
+
+  newnode->text = calloc(strlen(text)+1,sizeof(char));
+
+  strcpy(newnode->text,text);
+
+  newnode->next = Table[address];
+
+  Table[address] = newnode;
+
+  return newnode;
+
+}
+
+
+
+void hashPrint(void)
+{
+  int i;
+  
+  HASH *node;
+ 
+  for(i=0;i<HASH_SIZE;i++)
+    for(node = Table[i];node;node->next)
+      printf("Table [%d] has %s\n",i,node->text)
+}
+
---
+++ hash.h
@@ -0,0 +1,21 @@
+#ifndef __hash__
+#define __hash__
+
+#define SIZE 997
+/*
+Header da Tabela Hash, autoria de Marcelo Johann 2018 
+*/
+
+typedef struct hash_node{
+  int type;
+  char *text;
+  struct hash_node *next;
+}HASH;
+
+void hashInit(void);
+int hashAddress(char *text);
+HASH* hashInsert(int type, char *text);
+void hashPrint(void);
+
+
+#endif
