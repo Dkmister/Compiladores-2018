fa7e99a5625f130cc31a8dbfde4d07d04fe0badb
dasmig <diegomigotto@hotmail.com>
2018-03-18 14:43:36 -0300

    Functions reorganized in aux

--- aux.c
+++ aux.c
@@ -1,7 +1,9 @@
 #include "aux.h"
 #include "hash.h"
 
-void initMe(void)
-{
-  hashInit();
-}
+
+void initMe(void) { hashInit(); runningVar = 1; }
+
+int isRunning(void) { return runningVar; }
+
+void foundEof(void) { runningVar = 0; }
--- aux.h
+++ aux.h
@@ -3,4 +3,18 @@
 #ifndef __aux__
 #define __aux__
 
+/// Variables
+
+// Execution control var
+int runningVar;
+
+/// Functions
+
+// Initialization funcion
 void initMe(void);
+
+// Returns true for running and false for finished
+int isRunning(void);
+
+// Used by lex to tell that he has found EOF
+void foundEof(void);
