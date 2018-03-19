b26c84a90c74a27419fffcad393c7587ebfaebc6
dasmig <diegomigotto@hotmail.com>
2018-03-18 21:30:39 -0300

    It lives, main working, can input from file or stdin

--- main.c
+++ main.c
@@ -1,5 +1,25 @@
-#include <stdio.h>
+#include "main.h"
+extern FILE *yyin;
 
 
-int main()
-{return 0;}
+int main(int argc, char **argv)
+{
+
+  int token = 0;
+  
+  initMe();
+
+  if (argc == 1)
+    yyin = fopen(argv[1], "r");
+  else
+    yyin = stdin;
+
+  while(isRunning())
+  {
+
+    token = yylex();
+    printf("Line number: %d ;", getLineNumber());
+
+  }
+  return 0;
+}
