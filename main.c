627ba8be9ea8e4c66ae063e18b491605fdbdcedc
Dkmister <vilmar.neto@hotmail.com>
2018-03-21 17:47:19 -0300

    Alterado main para suportar arquivo

--- main.c
+++ main.c
@@ -6,19 +6,27 @@ int main(int argc, char **argv)
 {
 
   int token = 0;
-  
-  initMe();
 
-  if (argc == 1)
-    yyin = fopen(argv[1], "r");
-  else
-    yyin = stdin;
+
+
+  if (argc<2){
+    fprintf(stderr,"Chame pelamor de Deus : etapa1 nome_do_arquivo.\n ");
+    exit(1);
+  }
+
+  if( (yyin = fopen(argv[1]),"r") == 0)
+  {
+    fprintf("Nao pode abrir o arquivo %s, caramba >:( ",argv[1]);
+    exit(2);
+  }
+
+  initMe();
 
   while(isRunning())
   {
 
     token = yylex();
-    printf("Line number: %d ; Token: %d ; \n", getLineNumber(), token);
+    printf("Linha numero: %d ;\r Token: %d ; \n", getLineNumber(), token);
 
   }
   return 0;
